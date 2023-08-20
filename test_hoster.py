a6cae4e33cd80423beaca9321d8d00fe1781533b
lambotik <lambotik1988@gmail.com>
2023-08-20 13:23:52 +0300

    Finished Task

--- test_hoster.py
+++ test_hoster.py
@@ -7,14 +7,13 @@ from utils.checking import Checking
 
 from utils.reqres_api import HosterAPI
 
-"""Create, edit and delete a new user"""
 user_id = '2'  # default value
 
 
-@allure.feature('Test Hoster')
+@allure.feature('Test Hoster.')
 class TestHoster:
     @pytest.fixture()
-    @allure.step('test_create_new_user')
+    @allure.step('Test create new user.')
     def test_create_new_user(self):
         print('\n\nMethod POST: Create User')
         result_post = HosterAPI.create_new_user()
@@ -26,8 +25,7 @@ class TestHoster:
         Checking.check_json_search_word_in_value(result_post, 'job', 'qa')
         return new_id
 
-    @allure.step('test_info_user')
-    # @pytest.mark.xfail
+    @allure.step('Test new user info.')
     def test_info_new_user(self, test_create_new_user):
         print('\n\nMethod GET: Check user info new user')
         result_get = HosterAPI.checking_new_user(test_create_new_user)
@@ -40,9 +38,7 @@ class TestHoster:
                                                   "https://reqres.in/#support-heading")
         Checking.check_json_search_word_in_value(result_get_new_user['data'], 'id', test_create_new_user)
 
-    # @pytest.mark.xfail
-    @allure.step('test_info_user')
-    # @pytest.mark.xfail
+    @allure.step('Test default user info.')
     def test_info_default_user(self):
         print('\n\nMethod GET: Check user info default user')
         result_get = HosterAPI.checking_new_user(user_id)
@@ -57,7 +53,7 @@ class TestHoster:
                                                   "https://reqres.in/#support-heading")
         Checking.check_json_search_word_in_values(result_get_user['data'], 'id', user_id)
 
-    @allure.step('test_update_user_info')
+    @allure.step('Test update new user info.')
     def test_update_new_user_info(self, test_create_new_user):
         print('\n\nMethod PUT: Update new user data')
         result_put = HosterAPI.put_new_user(test_create_new_user)
@@ -66,7 +62,7 @@ class TestHoster:
         Checking.check_json_value(result_put, 'name', 'HOSTER')
         Checking.check_json_value(result_put, 'job', 'QA')
 
-    @allure.step('test_update_user_info')
+    @allure.step('Test update default user info.')
     def test_update_default_user_info(self):
         print('\n\nMethod PUT: Update default user data')
         result_put = HosterAPI.put_default_user(user_id)
@@ -75,13 +71,13 @@ class TestHoster:
         Checking.check_json_value(result_put, 'name', 'morpheus')
         Checking.check_json_value(result_put, 'job', 'zion resident')
 
-    @allure.step('test_delete_user')
+    @allure.step('Test delete default user.')
     def test_delete_default_user(self):
         print('\n\nMethod DELETE: Delete default user data')
         result_delete = HosterAPI.delete_new_user(user_id)
         Checking.check_status_code(result_delete, 204)
 
-    @allure.step('test_delete_user')
+    @allure.step('Test delete new user.')
     def test_delete_new_user(self, test_create_new_user):
         print('\n\nMethod DELETE: Delete new user data')
         result_delete = HosterAPI.delete_new_user(test_create_new_user)
